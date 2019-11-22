11e0160e804667198bb533c53b4cd2c24a647c90
JakeAsh22 <jacob.ash1998@gmail.com>
2019-11-22 12:59:41 -0800

    Will allow values to be pulled from api without receiving errors

--- src/main.js
+++ src/main.js
@@ -8,11 +8,17 @@ $(document).ready(function()
 {
   $("#doctType").click(function()
   {
+    let name = "Seattle Childrens";
+    let doctor = new Doctor();
     (async () =>
     {
       const response = await doctor.getDocName();
       let docName = getElements(response);
       console.log(docName);
-    });
-  })
+    })();
+    const getElements = function (response)
+    {
+      return response[name];
+    }
+  });
 });
