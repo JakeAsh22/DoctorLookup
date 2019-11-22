974705faf6909fa6b939b4e219f50f9295556301
JakeAsh22 <jacob.ash1998@gmail.com>
2019-11-22 11:41:54 -0800

    Think I finally understand how to use this API correctly.  This will make basic specs for what I want my website to do.

---
+++ README.md
@@ -0,0 +1,7 @@
+specs:
+-User should be able to find a doctor for what they need: ie cancer, common illness, etc.
+-Search doctors by name
+-show the doctors info on the page
+-make sure errors are returned correctly
+-if no doctors fit, let user know
+-also provide information if that the user may have misspelled their info
--- src/doctor.js
+++ src/doctor.js
@@ -1,6 +1,6 @@
 export class Doctor {
   async getDocName(){
-    let response = await fetch ('https://api.betterdoctor.com/2016-03-01/doctors?skip=0&limit=10&user_key=99c49e507ab58e761efd1622ee667e9f
+    let response = await fetch ('https://api.betterdoctor.com/2016-03-01/practices?location=wa-seattle&skip=0&limit=10&user_key=99c49e507ab58e761efd1622ee667e9f
 ')
     let jsonifiedResponse = await response.json();
     return jsonifiedResponse;
