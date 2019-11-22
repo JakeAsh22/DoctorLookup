94a2b9758b6036779deba0da3c0f9c9d1cd76e47
JakeAsh22 <jacob.ash1998@gmail.com>
2019-11-22 14:25:02 -0800

    WIll make some of the program to finally work, like basic stages of getElements

--- src/main.js
+++ src/main.js
@@ -2,16 +2,33 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { Doctor } from './doctor.js';
+//import { Doctor } from './doctor.js';
 
 $(document).ready(function()
 {
   $("#doctType").click(function()
   {
     let name = "Varley";
-    let doctor = new Doctor();
+    //let doctor = new Doctor();
     let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${name}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=99c49e507ab58e761efd1622ee667e9f`
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${name}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=99c49e507ab58e761efd1622ee667e9f`;
+
+    request.onreadystatechange = function()
+    {
+      if (this.readyState ===4 && this.status ===200) {
+        let response = JSON.parse(this.responseText);
+        console.log(response);
+        getElements(response);
+      }
+
+    };
+
+    request.open("GET",url,true);
+    request.send();
+
+    const getElements = function(response)
+    {
+      $(".docstuff").text(`${name}`);
     };
   });
 });
