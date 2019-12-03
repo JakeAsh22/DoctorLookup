f4fb0582085617a9e4da11be8ba52746d08d30e6
JakeAsh22 <jacob.ash1998@gmail.com>
2019-12-02 19:34:04 -0800

    Will make the results of one doctor serached by last name appear

--- src/main.js
+++ src/main.js
@@ -6,14 +6,14 @@ import $ from 'jquery';
 
 $(document).ready(function()
 {
-  $("#doctType").click(function()
+  $("#nameButton").click(function()
   {
     $(".doctorCard").hide();
     $(".doctorCard").show();
     //let name = "Varley";
     //let doctor = new Doctor();
     let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=wa-seattle&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=Smith&location=wa-seattle&skip=0&limit=50&user_key=${process.env.API_KEY}`;
 
     request.onreadystatechange = function()
     {
@@ -24,12 +24,18 @@ $(document).ready(function()
       }
 
     };
-
+    let newPatients = "";
     request.open("GET",url,true);
     request.send();
     const getElements = function(response)
     {
-      $(".docstuff").text(`${response.practices}`);
+      console.log(response.data[1]);
+      if (response.data[0].practices[0].accepts_new_patients)
+        newPatients = "Is accepting new patients";
+      else
+        newPatients = "Is not accepting new patients"
+      console.log(response.data[0].practices[0].phones[0].number);
+      $(".docstuff").text(`The doctors information is: ${response.data[0].profile.last_name}, ${response.data[0].profile.first_name} Phone Number: ${response.data[0].practices[0].phones[0].number} Accepting new patients? ${newPatients}`);
     };
   });
 });
