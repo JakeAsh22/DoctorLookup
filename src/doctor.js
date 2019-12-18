7db62ced5ee6e3f39738226e4bffc298a5ed36b4
JakeAsh22 <jacob.ash1998@gmail.com>
2019-12-17 18:29:55 -0800

    Will seperate logic, have results print correctly, and get both buttons functional

--- src/doctor.js
+++ src/doctor.js
@@ -1,17 +1,29 @@
 export class Doctor
 {
-  async getDocName()
+  async getDocByName(lastName)
   {
     try
     {
-
-    let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?last_name=Varley&location=wa-seattle&skip=0&limit=10&user_key=${process.env.API_KEY}`);
-    let jsonifiedResponse = await response.json();
-    return jsonifiedResponse;
+      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastName}&location=wa-seattle&skip=0&limit=50&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
     }
     catch (error)
     {
-      return "There are no Doctors in your area with that Last Name";
+      return "Sorry, there is an error calling this database.  Please try again";
+    }
+  }
+  async getDocByIssue(issue)
+  {
+    try
+    {
+      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=wa-seattle&skip=0&limit=50&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    }
+    catch (error)
+    {
+      return "Sorry, there is an error calling this database.  Please try again";
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,40 +2,83 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-//import { Doctor } from './doctor.js';
+import { Doctor } from './doctor.js';
 
 $(document).ready(function()
 {
   $("#nameButton").click(function()
   {
+    $(".docstuff").text("");
     $(".doctorCard").hide();
     $(".doctorCard").show();
-    //let name = "Varley";
-    //let doctor = new Doctor();
-    let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=Smith&location=wa-seattle&skip=0&limit=50&user_key=${process.env.API_KEY}`;
-
-    request.onreadystatechange = function()
-    {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        //console.log("hello");
-        getElements(response);
-      }
-
-    };
+    let lastName = $('#docFind').val();
     let newPatients = "";
-    request.open("GET",url,true);
-    request.send();
+
+    (async () => {
+      let lastNameSearch = new Doctor();
+      const response = await lastNameSearch.getDocByName(lastName);
+      getElements(response);
+    })();
+
     const getElements = function(response)
     {
-      console.log(response.data[1]);
-      if (response.data[0].practices[0].accepts_new_patients)
-        newPatients = "Is accepting new patients";
+      if (lastName == '')
+      {
+        $(".docstuff").text("Please enter the last name of a doctor.");
+      }
       else
-        newPatients = "Is not accepting new patients"
-      console.log(response.data[0].practices[0].phones[0].number);
-      $(".docstuff").text(`The doctors information is: ${response.data[0].profile.last_name}, ${response.data[0].profile.first_name} Phone Number: ${response.data[0].practices[0].phones[0].number} Accepting new patients? ${newPatients}`);
+      {
+        for (let i = 0;i<response.data.length;i++)
+        {
+          if (response.data[i].practices[0].accepts_new_patients)
+            newPatients = "Yes";
+          else
+            newPatients = "No";
+
+          $(".docstuff").append(`<li>The doctors information is:
+            ${response.data[i].profile.last_name}, ${response.data[i].profile.first_name}
+           <ul>Phone Number: ${response.data[i].practices[0].phones[0].number}</ul>
+           <ul>Accepting new patients? ${newPatients}</ul></li>`);
+        }
+      }
     };
   });
+
+  $("#sickButton").click(function()
+  {
+    $(".docstuff").text("");
+    $(".doctorCard").hide();
+    $(".doctorCard").show();
+    let issue = $('#sickness').val();
+    let newPatients = "";
+
+    (async () => {
+      let issueSearch = new Doctor();
+      const response = await issueSearch.getDocByIssue(issue);
+      getElements(response);
+    })();
+    const getElements = function(response)
+    {
+      if (issue == '')
+      {
+        $(".docstuff").text("Please enter the issue you are having.");
+      }
+      else
+      {
+        for (let i = 0;i<response.data.length;i++)
+        {
+          if (response.data[i].practices[0].accepts_new_patients)
+            newPatients = "Yes";
+          else
+            newPatients = "No";
+
+          $(".docstuff").append(`<li>The doctors information is:
+            ${response.data[i].profile.last_name}, ${response.data[i].profile.first_name}
+           <ul>Phone Number: ${response.data[i].practices[0].phones[0].number}</ul>
+           <ul>Accepting new patients? ${newPatients}</ul></li>`);
+        }
+      }
+    };
+  });
+
 });
